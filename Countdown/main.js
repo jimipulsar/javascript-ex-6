7a882929896aa6d73f5f99c8e83b4a507091e736
jimipulsar <jimipulsar@gmail.com>
2018-11-27 13:50:40 +0100

    Esercizio corretto e completato- Timing Function

--- Countdown/main.js
+++ Countdown/main.js
@@ -1,161 +1,72 @@
 // Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi. Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che sono stati inseriti 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-//generiamo un alert con 5 numeri casuali
+
 var numeri = [];
 var i = 0;
-while( i < 5){
- var n = Math.floor((Math.random() * 90) +1);
- var r = numeri.indexOf(n);
- if(r < 0){
- numeri.push(n);
- console.log(numeri);
- i++;
- }
+var stringa_alert = "";
+
+while (i < 5) {
+  var n = Math.floor((Math.random() * 90) +1);
+  if (!numeri.includes(n)) {
+    numeri.push(n);
+    stringa_alert += n;
+    if(i < 5 - 1){
+      stringa_alert += '  ';
+    }
+    i++;
+  }
 }
-alert("Ecco 5 numeri casuali: " + numeri);
+console.log(stringa_alert);
+alert(stringa_alert);
 
 
 var secondi = 30;
 var elem = document.getElementById('countdown');
+var risultato_elem = document.getElementById('risultato');
 var tempo;
+var quantitanumeri = 5;
 
-var timerId = setInterval(countdown, 1000);
 
-function countdown() {
-  if (secondi == 0) {
-    clearTimeout(timerId);
-  } else {
-    elem.innerHTML = secondi + ' secondi rimasti';
-    secondi--;
-  }
-  if (secondi == 0) {
-      tempo = document.getElementById('countdown');
-      tempo.innerHTML = "Vai";
-      return;
+var countDown = setInterval(countDownFunction, 1000);
+
+function countDownFunction(){
+  var h1 = document.getElementById("secondi");
+  h1.innerHTML = secondi;
+    if(secondi == 0){
+        clearInterval(countDown);
+        var promptutente = promptUser(quantitanumeri);
+        var confronto = confronta_array(numeri, promptutente);
+        console.log(confronto);
+        var risultato = document.getElementById("risultato");
+        risultato.innerHTML = "Hai indovinato " + confronto.length + " numeri <br>";
     }
+    secondi--;
 }
 
-
-var numero_utente = prompt("Inserisci il primo numero:");
-var i = 0;
-var trovato = false;
-for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] == numero_utente) {
-    trovato = true;
+function promptUser(quantitanumeri) {
+  var numeri_utenti = [];
+  var messaggio = "Inserisci un numero";
+  while(numeri_utenti.length < quantitanumeri) {
+    var numero_utente = parseInt(prompt(messaggio));
+    if(!numeri_utenti.includes(numero_utente)) {
+      console.log(numero_utente);
+      numeri_utenti.push(numero_utente);
+      messaggio = "Inserisci un numero";
+    }
+    else {
+      messaggio = "Hai già inserito questo numero. Inseriscine un altro";
+    }
   }
-}
-if (trovato == true) {
-  var notifica1 = document.getElementById('numerorichiesto');
-  document.writeln("Hai indovinato il numero:" + numero_utente +"<br>") ;
-  console.log("Accesso consentito.");
-}
-else {
-  var notifica2 = document.getElementById('numerorichiesto');
-  console.log("Accesso negato.");
+  console.log(numeri_utenti);
+  return numeri_utenti;
 }
 
-var numero_utente = prompt("Inserisci il secondo numero:");
-var i = 0;
-var trovato = false;
-for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] == numero_utente) {
-    trovato = true;
+function confronta_array(array1, array2) {
+  var numeri_indovinati = [];
+  for (var i = 0; i < array1.length; i++) {
+    if (array2[i] == array1[i]) {
+      numeri_indovinati.push(array2[i]);
+    }
   }
+  return numeri_indovinati;
 }
-if (trovato == true) {
-  var notifica1 = document.getElementById('numerorichiesto');
-  document.writeln("Hai indovinato il numero:" + numero_utente) ;
-  console.log("Accesso consentito.");
-}
-else {
-  var notifica2 = document.getElementById('numerorichiesto');
-  console.log("Accesso negato.");
-}
-
-var numero_utente = prompt("Inserisci il terzo numero:");
-var i = 0;
-var trovato = false;
-for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] == numero_utente) {
-    trovato = true;
-  }
-}
-if (trovato == true) {
-  var notifica1 = document.getElementById('numerorichiesto');
-  document.writeln("Hai indovinato il numero:" + numero_utente) ;
-  console.log("Accesso consentito.");
-}
-else {
-  var notifica2 = document.getElementById('numerorichiesto');
-  console.log("Accesso negato.");
-}
-
-var numero_utente = prompt("Inserisci il quarto numero:");
-var i = 0;
-var trovato = false;
-for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] == numero_utente) {
-    trovato = true;
-  }
-}
-if (trovato == true) {
-  var notifica1 = document.getElementById('numerorichiesto');
-  document.writeln("Hai indovinato il numero:" + numero_utente) ;
-  console.log("Accesso consentito.");
-}
-else {
-  var notifica2 = document.getElementById('numerorichiesto');
-  console.log("Accesso negato.");
-}
-
-var numero_utente = prompt("Inserisci il quinto numero:");
-var i = 0;
-var trovato = false;
-for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] == numero_utente) {
-    trovato = true;
-  }
-}
-if (trovato == true) {
-  var notifica1 = document.getElementById('numerorichiesto');
-  document.writeln("Hai indovinato il numero:" + numero_utente) ;
-  console.log("Accesso consentito.");
-}
-else {
-  var notifica2 = document.getElementById('numerorichiesto');
-  console.log("Accesso negato.");
-}
-
-/*
-var i = 0;
-var numeri_random = [];
-
-
-var secondi = 30;
-
-var countdown = setInterval(numerigenerati, 3000);
-function numerigenerati() {
-  document.writeln("Mancano " + secondi + " secondi");
-console.log(secondi);
-if(secondi == 0) {
-  clearInterval(countdown);
-  console.log("bene");
-}
-secondi -= 1;
-}
-
-
-
-/*
-setTimeout(messaggio, 3000);
-function messaggio() {
-console.log("Ciao");
-alert("Ciao Classe");
-}
-console.log("Ciao Console log");
-
-function getRandom(min, max){
-  var random = Math.floor(Math.random() * (max - min + 1)+ min);
-  return random;
-}
-*/
